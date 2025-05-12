38f1cb46cfa665d30cc25910452533f6b05d0fb1
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-12 11:43:55 +0200

    feat: create basic user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """Represents a worker being tracked."""
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
