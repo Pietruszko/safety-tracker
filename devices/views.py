d8fd66cfc4a2dd1bd055244a1b4ec3d461cfa342
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-13 12:18:12 +0200

    feat: create device view set with custom action for devices/<id>/assign endpoint

--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Device
+from .serializers import DeviceSerializer, AssignDeviceSerializer, LocationPingSerializer
 
-# Create your views here.
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+    @action(detail=True, methods=['post'], url_path='assign')
+    def assign(self, request, pk=None):
+        device = self.get_object()
+        serializer = AssignDeviceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(device=device)
+            return Response({'detail': 'Device assigned successfully.'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
