efef96f3afbfcef82ce6cb845be8f2ee1dd69c00
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-14 20:57:42 +0200

    feat: create user viewset and custom action for users/id/location endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'], url_path='location')
+    def location(self, request, pk=None):
+        user = self.get_object()
+
+        device = user.devices.filter(is_active=True).first()
+        if not device:
+            return Response({"detail": "User has no active device."}, 
+                            status=status.HTTP_404_NOT_FOUND)
+        
+        last_ping = device.location_pings.order_by('-ping_time').first()
+        if not last_ping:
+            return Response({"detail": "No location data available."}, 
+                            status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({
+            "latitude": last_ping.latitude,
+            "longitude": last_ping.longitude,
+            "timestamp": last_ping.ping_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        }, status=status.HTTP_200_OK)
